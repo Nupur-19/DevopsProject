9848af2cd9ea3f6fa677e9f6fa362da9a0c9f3c4
Ubuntu <nupur.bajaj@vodafone.com>
2020-03-30 13:34:56 +0000

    Committing DevopsProject Files

---
+++ DevopsProject.c
@@ -0,0 +1,5 @@
+#include<studio.h>
+int main(){
+printf(" Hello I am C Program in Devops Project. Thanks!");
+return 0;
+}
---
+++ DevopsProject.java
@@ -0,0 +1,5 @@
+class DevopsProject{
+public static void main(String Args[]){
+System.out.Println("I am your Devops Java project")
+}
+}
